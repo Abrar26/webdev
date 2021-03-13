23668126542e2d9ea7f7c54eb66f80d30d6161ec
Abrar <abrargd1695@gmail.com>
2021-03-13 14:10:25 +0530

    Python file about class basic Example

--- first.py
+++ first.py
@@ -1,6 +1,14 @@
-with open("txt2.txt","w") as f:
-    for i in range(1,21):
-        for j in range(1,11):
-            f.write(f"{i}x{j}={i*j}\n")    
-            
-        f.write("\n")
+class Employee():
+    #Contructor Example
+    def __init__(self):
+        print("Constructor is created")
+    
+    #Self object Example
+    def getInformation(self):
+        print(f"{self.name} is a student of {self.clg} and age of the student is {self.age}.")
+        
+Abrar=Employee()
+Abrar.name="Abrar Ahmed"
+Abrar.age=20
+Abrar.clg="CAHCET"
+Abrar.getInformation()
