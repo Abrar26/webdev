6426171ee31d716aa97bcd84c096f5d436462372
Abrar <abrargd1695@gmail.com>
2021-03-21 21:08:50 +0530

    Guess The Number Game Using Class

--- first.py
+++ first.py
@@ -1,13 +1,34 @@
 import random
-class Train:
-    def __init__(self,name,fare,seats):
-        self.name=name
-        self.fare=fare
-        self.seats=seats
-        
-    def getStatus(self):
-        print(f"The name of the train is {self.name}")
-        print(f"The seats available in the train are {random.randint(0,self.seats)}")
+rd=random.randint(1,10)
 
-lalbagh= Train("LALBAGH",60,300)
-lalbagh.getStatus()
+class Game:
+    count=1
+    def __init__(self,rd):
+        self.rdn=rd
+    def guess(self,val):
+        bools=True
+        while(bools):
+            if(val==self.rdn):
+                print(f"FU*K YOU GUESSED CORRECT After the {self.count} guess")
+                break
+            elif (val>self.rdn):
+                print(r"LOL! YOUR NUMBER IS TOO HIGH :)")
+                self.count+=1
+            else:
+                print("LOL! YOUR NUMBER IS TOO LOW :)")
+                self.count+=1
+            
+            val=int(input("Guess again: "))
+            
+        
+
+gm=Game(rd)
+n=int(input("Guess the Number: "))
+op=gm.guess(n)
+with open("txt2.txt","r") as f:
+    hs=(f.readline())
+    if(int(hs)>gm.count):
+        with open("txt2.txt","w") as f:
+            print("YOU HAVE BROKEN THE HIGH_SCORE")
+            f.write(f"{gm.count}")
+    
