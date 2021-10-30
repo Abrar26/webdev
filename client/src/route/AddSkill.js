828044a9d75485a9ef6a32622eb12e5373d1d012
Abrar <abrargd1695@gmail.com>
2021-10-30 23:01:25 +0530

    COmpleted in Home Page

--- client/src/route/AddSkill.js
+++ client/src/route/AddSkill.js
@@ -1,22 +1,43 @@
 
 import React, { useRef, useState } from "react";
-// Import Swiper React components
-
-// Import Swiper styles
-import 'swiper/swiper.min.css'
-// import Swiper core and required modules
-import SwiperCore, {
-  Pagination
-} from 'swiper';
-
-// install Swiper modules
-SwiperCore.use([Pagination]);
+import { Button,
+  FormControl,
+  InputLabel,
+  Input,
+  TextField,
+  Box
+} from '@mui/material';
 
 function AddSkill(){
     
     return (
         <>
-        
+        <div className="addskill">
+          <h2>Add Your Skill</h2>
+          <FormControl>
+            <TextField id="outlined-basic" size="small" label="Outlined" variant="outlined" />
+            <InputLabel htmlFor="my-input">Email address</InputLabel>
+            <Input id="my-input" variant="outlined" aria-describedby="my-helper-text" />
+            <Button color="primary" variant="contained" >Submit</Button>
+          </FormControl>
+
+          <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Outlined"  />
+    </Box>
+    <TextField
+          required
+          id="outlined-required"
+          label="Required"
+          defaultValue="Hello World"
+        />
+        </div>
         </>
     )
     }
